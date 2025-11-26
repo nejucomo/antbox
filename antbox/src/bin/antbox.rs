66936c78a62235a9d69074f545b9f42a5595dd55
nobody <nobody@nowhere.net>
2025-11-26 07:19:11 -0700

    Enable full-screen.

--- antbox/src/bin/antbox.rs
+++ antbox/src/bin/antbox.rs
@@ -1,5 +1,6 @@
+use antbox::AntBox;
 use pix_engine::error::Result;
 
 fn main() -> Result<()> {
-    antbox::run()
+    AntBox::run()
 }
--- antbox/src/lib.rs
+++ antbox/src/lib.rs
@@ -1,5 +1,44 @@
-mod gstate;
-mod run;
+use pix_engine::color::Color;
+use pix_engine::engine::{Engine, PixEngine};
+use pix_engine::error::Result;
+use pix_engine::event::Key;
+use pix_engine::prelude::Font;
+use pix_engine::state::PixState;
 
-pub use self::gstate::GameState;
-pub use self::run::run;
+#[derive(Debug, Default)]
+pub struct AntBox {}
+
+impl AntBox {
+    pub fn run() -> Result<()> {
+        let mut engine = Engine::builder()
+            .title(env!("CARGO_PKG_NAME"))
+            .target_frame_rate(5)
+            .dimensions(800, 600)
+            .fullscreen()
+            .build()?;
+
+        let mut app = AntBox::default();
+        engine.run(&mut app)
+    }
+}
+
+impl PixEngine for AntBox {
+    fn on_start(&mut self, ps: &mut PixState) -> Result<()> {
+        ps.background(Color::GRAY);
+        ps.font_family(Font::NOTO)?;
+        ps.font_size(16)?;
+        Ok(())
+    }
+
+    fn on_update(&mut self, ps: &mut PixState) -> Result<()> {
+        if ps.key_down(Key::Escape) {
+            ps.quit();
+        } else {
+            // Draw a circle with fill color at the mouse position with a radius of
+            // 80.
+            let m = ps.mouse_pos();
+            ps.circle([m.x(), m.y(), 80])?;
+        }
+        Ok(())
+    }
+}
