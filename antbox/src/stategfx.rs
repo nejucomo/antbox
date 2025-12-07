a3f237bb3f3f317d2536bbce23cde2a60b5185be
nobody <nobody@nowhere.net>
2025-12-06 16:49:18 -0800

    Split out `StateGfx` internal methods into "layer" rendering.

--- antbox/src/stategfx.rs
+++ antbox/src/stategfx.rs
@@ -14,20 +14,22 @@ impl StateGfx {
         let bounds = self.0.food.bounds();
         let w32 = bounds.width as f32;
         let h32 = bounds.height as f32;
-        let cell_width = viewsize.x / w32;
-        let cell_height = viewsize.y / h32;
+        let cellsize = Vec2::new(viewsize.x / w32, viewsize.y / h32);
 
         graphics.clear_screen(colors::BACKGROUND);
 
-        log::debug!("drawing popped food grid");
+        self.draw_food_life(graphics, cellsize);
+    }
+
+    fn draw_food_life(&self, graphics: &mut Graphics2D, cellsize: Vec2) {
         for (pt, cell) in self.0.food.iter() {
             if cell.is_alive() {
                 graphics.draw_circle(
                     (
-                        cell_width * (pt.x() as f32) + cell_width / 2.0,
-                        cell_height * (pt.y() as f32) + cell_height / 2.0,
+                        cellsize.x * (pt.x() as f32) + cellsize.x / 2.0,
+                        cellsize.y * (pt.y() as f32) + cellsize.y / 2.0,
                     ),
-                    cell_width.min(cell_height) / 2.0,
+                    cellsize.x.min(cellsize.y) / 2.0,
                     colors::FOOD,
                 );
             }
