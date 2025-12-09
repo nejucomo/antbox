3672c5869b01f72ddf8c2871d2795a37373be61e
nobody <nobody@nowhere.net>
2025-12-09 14:56:14 -0800

    Simplify existing rendering; replace `RenderRound` with `RenderMetrics`.

--- antbox/src/anim.rs
+++ antbox/src/anim.rs
@@ -16,32 +16,6 @@ pub(crate) struct AnimationState {
     state: State,
 }
 
-impl AnimationState {
-    pub(crate) fn draw(&self, gfx: &mut Graphics2D, view_size: Vec2) {
-        let state = &self.state;
-
-        let food_cell_bounds = {
-            let bounds = state.food.bounds();
-            let w32 = bounds.width as f32;
-            let h32 = bounds.height as f32;
-
-            Vec2::new(view_size.x / w32, view_size.y / h32)
-        };
-
-        let rr = RenderRound {
-            state,
-            view_size,
-            food_cell_bounds,
-            food_cell_radius: food_cell_bounds.x.min(food_cell_bounds.y) / 2.0,
-        };
-
-        rr.draw_background(gfx);
-        rr.draw_food_pods(gfx);
-        rr.draw_food_life(gfx);
-        rr.draw_wire_frame(gfx);
-    }
-}
-
 impl UpdateInput<Tick> for AnimationState {
     fn update_input(mut self, _: Tick) -> Self {
         self.state = self.state.into_next();
@@ -49,34 +23,55 @@ impl UpdateInput<Tick> for AnimationState {
     }
 }
 
-struct RenderRound<'a> {
-    state: &'a State,
+#[derive(Debug)]
+struct RenderMetrics {
     view_size: Vec2,
     food_cell_bounds: Vec2,
     food_cell_radius: f32,
 }
 
-impl<'a> RenderRound<'a> {
-    fn draw_background(&self, g: &'a mut Graphics2D) {
+impl AnimationState {
+    pub(crate) fn draw(&self, gfx: &mut Graphics2D, view_size: Vec2) {
+        let food_cell_bounds = {
+            let bounds = self.state.food.bounds();
+            let w32 = bounds.width as f32;
+            let h32 = bounds.height as f32;
+
+            Vec2::new(view_size.x / w32, view_size.y / h32)
+        };
+
+        let rm = &RenderMetrics {
+            view_size,
+            food_cell_bounds,
+            food_cell_radius: food_cell_bounds.x.min(food_cell_bounds.y) / 2.0,
+        };
+
+        self.draw_background(gfx, rm);
+        self.draw_food_pods(gfx, rm);
+        self.draw_food_life(gfx, rm);
+        self.draw_wire_frame(gfx, rm);
+    }
+
+    fn draw_background(&self, g: &mut Graphics2D, _: &RenderMetrics) {
         g.clear_screen(colors::BACKGROUND);
     }
 
-    fn draw_wire_frame(&self, g: &'a mut Graphics2D) {
+    fn draw_wire_frame(&self, g: &mut Graphics2D, rm: &RenderMetrics) {
         let Bounds { width, height } = self.state.food.bounds();
         for col in 0..width {
-            let x = (col as f32) * self.food_cell_bounds.x;
-            g.draw_line((x, 0.0), (x, self.view_size.y), 1.0, colors::WIRE_FRAME);
+            let x = (col as f32) * rm.food_cell_bounds.x;
+            g.draw_line((x, 0.0), (x, rm.view_size.y), 1.0, colors::WIRE_FRAME);
         }
         for row in 0..height {
-            let y = (row as f32) * self.food_cell_bounds.y;
-            g.draw_line((0.0, y), (self.view_size.x, y), 1.0, colors::WIRE_FRAME);
+            let y = (row as f32) * rm.food_cell_bounds.y;
+            g.draw_line((0.0, y), (rm.view_size.x, y), 1.0, colors::WIRE_FRAME);
         }
     }
 
-    fn draw_food_pods(&self, g: &'a mut Graphics2D) {
-        let crad = self.food_cell_radius * 0.9; // crowded flavor
+    fn draw_food_pods(&self, g: &mut Graphics2D, rm: &RenderMetrics) {
+        let crad = rm.food_cell_radius * 0.9; // crowded flavor
 
-        for (pt, center) in self.iter_pts_and_centers() {
+        for (pt, center) in self.iter_pts_and_centers(rm) {
             let cnt = self.state.food.neighbor_counts()[pt];
             if cnt > 0 {
                 let cellrotation = Angle::from(center.magnitude());
@@ -101,17 +96,17 @@ impl<'a> RenderRound<'a> {
         }
     }
 
-    fn draw_food_life(&self, g: &mut Graphics2D) {
-        for (pt, center) in self.iter_pts_and_centers() {
+    fn draw_food_life(&self, g: &mut Graphics2D, rm: &RenderMetrics) {
+        for (pt, center) in self.iter_pts_and_centers(rm) {
             let cell = self.state.food.life()[pt];
             if cell.is_alive() {
-                g.draw_circle(center, self.food_cell_radius / 2.0, colors::FOODLIFE);
+                g.draw_circle(center, rm.food_cell_radius / 2.0, colors::FOODLIFE);
             }
         }
     }
 
-    fn iter_pts_and_centers(&self) -> impl Iterator<Item = (BoundPoint, Vec2)> {
-        let Vec2 { x: cellw, y: cellh } = self.food_cell_bounds;
+    fn iter_pts_and_centers(&self, rm: &RenderMetrics) -> impl Iterator<Item = (BoundPoint, Vec2)> {
+        let Vec2 { x: cellw, y: cellh } = rm.food_cell_bounds;
 
         self.state.food.bounds().iter_points().map(move |pt| {
             (
