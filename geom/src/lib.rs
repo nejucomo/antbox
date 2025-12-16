cfa2a550d2dd1ce891617b8fddd531ae576f992b
nobody <nobody@nowhere.net>
2025-12-15 19:06:11 -0800

    Remove unused `Rect` type.

--- geom/src/lib.rs
+++ geom/src/lib.rs
@@ -6,11 +6,9 @@ mod bounds;
 mod direction;
 mod grid;
 mod point;
-mod rect;
 
 pub use crate::boundpoint::BoundPoint;
 pub use crate::bounds::Bounds;
 pub use crate::direction::Direction;
 pub use crate::grid::Grid;
 pub use crate::point::Point;
-pub use crate::rect::Rect;
