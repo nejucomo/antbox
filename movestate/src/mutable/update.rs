cea386afc8b9390c11f91ad509aa8828762bf8fb
nobody <nobody@nowhere.net>
2025-12-23 16:23:35 +0900

    Introduce `Update::into_take_into_starg`.

--- movestate/src/mutable/update.rs
+++ movestate/src/mutable/update.rs
@@ -1,5 +1,15 @@
+use crate::mutable::UpdateAsTakeIntoStarg;
+
 /// Mutably update and input `I` to produce an `O`
 pub trait Update<I, O> {
     /// Mutable update with `input` to produce a `O`
     fn update(&mut self, input: I) -> O;
+
+    /// Convert into a wrapper type which provides [TakeIntoStarg](crate::take_into::TakeIntoStarg)
+    fn into_take_into_starg(self) -> UpdateAsTakeIntoStarg<Self, I, O>
+    where
+        Self: Sized,
+    {
+        UpdateAsTakeIntoStarg::new(self)
+    }
 }
