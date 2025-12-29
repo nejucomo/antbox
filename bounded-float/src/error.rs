957375efbb1a7f862afb434d54b1411d42b2089b
nobody <nobody@nowhere.net>
2025-12-29 19:36:12 +0900

    Derive `thiserror::Error` for `BoundedFloatError`.

--- bounded-float/src/error.rs
+++ bounded-float/src/error.rs
@@ -1,5 +1,8 @@
+use thiserror::Error;
+
 /// An error involving an [f32] value
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Error)]
+#[error("BoundedFloatError for {f:?}")]
 pub struct BoundedFloatError {
     /// The value in question
     pub f: f32,
