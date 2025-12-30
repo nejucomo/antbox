cd5e37fc928451cfbf0280032ad2181fe3fd89c0
nobody <nobody@nowhere.net>
2025-12-31 08:25:03 +0900

    Overhaul `...-color` to be just one type.

--- color/src/lib.rs
+++ color/src/lib.rs
@@ -3,54 +3,51 @@
 
 use antbox_float::NormF32;
 
-macro_rules! def_rgba {
-    ( $t:ident, $band:ty, $maker_alpha:ident, $maker_noa:ident, $default_alpha:expr ) => {
-        #[doc = concat!("An _RGBA_ color representation with [", stringify!($band), "] bands")]
-        pub struct $t {
-            /// The red band
-            pub r: $band,
-            /// The green band
-            pub g: $band,
-            /// The blue band
-            pub b: $band,
-            /// The alpha band
-            pub a: $band,
-        }
-
-        impl $t {
-            /// Construct with all four bands
-            pub const fn new(r: $band, g: $band, b: $band, a: $band) -> Self {
-                Self { r, g, b, a }
-            }
-
-            /// Construct with all _RGB_ bands and full alpha
-            pub const fn new_full_alpha(r: $band, g: $band, b: $band) -> Self {
-                Self::new(r, g, b, $default_alpha)
-            }
-        }
-
-        #[doc = concat!("A shorthand for [", stringify!($t), "::new]")]
-        pub const fn $maker_alpha(r: $band, g: $band, b: $band, a: $band) -> $t {
-            $t::new(r, g, b, a)
-        }
-
-        #[doc = concat!("A shorthand for [", stringify!($t), "::new_full_alpha]")]
-        pub const fn $maker_noa(r: $band, g: $band, b: $band) -> $t {
-            $t::new_full_alpha(r, g, b)
-        }
-    };
+/// An _RBGA_ color representation with [NormF32] bands
+pub struct RBGA {
+    /// The red band
+    pub r: NormF32,
+    /// The green band
+    pub g: NormF32,
+    /// The blue band
+    pub b: NormF32,
+    /// The alpha band
+    pub a: NormF32,
 }
 
-def_rgba!(RGBAu, u8, rgba, rgb, u8::MAX);
-def_rgba!(RGBAf, NormF32, rgba_f, rgb_f, NormF32::from_f32(1.0));
-
-impl From<RGBAu> for RGBAf {
-    fn from(us: RGBAu) -> Self {
-        RGBAf {
-            r: NormF32::from_u8(us.r),
-            g: NormF32::from_u8(us.g),
-            b: NormF32::from_u8(us.b),
-            a: NormF32::from_u8(us.a),
-        }
+impl RBGA {
+    /// Construct with all four bands
+    pub const fn new(r: NormF32, g: NormF32, b: NormF32, a: NormF32) -> Self {
+        Self { r, g, b, a }
     }
+
+    /// Construct with all _RGB_ bands and full alpha
+    pub const fn new_full_alpha(r: NormF32, g: NormF32, b: NormF32) -> Self {
+        Self::new(r, g, b, NormF32::from_f32(1.0))
+    }
+
+    /// Construct from [u8] bands
+    pub const fn new_u8(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Self::new(
+            NormF32::from_u8(r),
+            NormF32::from_u8(g),
+            NormF32::from_u8(b),
+            NormF32::from_u8(a),
+        )
+    }
+
+    /// Construct from  _RGB_ [u8] bands
+    pub const fn new_u8_full_alpha(r: u8, g: u8, b: u8) -> Self {
+        Self::new_u8(r, g, b, u8::MAX)
+    }
+}
+
+/// Shorthand for [RBGA::new_u8]
+pub const fn rgba(r: u8, g: u8, b: u8, a: u8) -> RBGA {
+    RBGA::new_u8(r, g, b, a)
+}
+
+/// Shorthand for [RBGA::new_u8_full_alpha]
+pub const fn rgb(r: u8, g: u8, b: u8) -> RBGA {
+    RBGA::new_u8_full_alpha(r, g, b)
 }
